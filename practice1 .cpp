d47201e6eaf7f09fb244902dae1324fdf0efa176
Junya Zhao <31264540+JunyaZ@users.noreply.github.com>
2018-04-16 18:06:32 -0500

    Update and rename lab1 .cpp to practice1 .cpp

--- practice1 .cpp
+++ practice1 .cpp
@@ -1,8 +1,27 @@
 /*==============================================================
- course: CSC232 Lab1
  Created by: Junya Zhao
  Last modified date :	1/30/2018
  Program:  8 function calculator
+ The program will display on the screen a menu of the eight operations telling the user to enter 
+ +, -, *, /, ^, l, !, or q to specify the operation to be performed. The program will read the
+ operation from the keyboard. If the user enters a q the execution will terminate. Otherwise, 
+ the program will display on the screen a prompt for the first operand, which it will then read 
+ from the keyboard. If the operation requires two operands then the program will display on the 
+ screen a prompt for the second operand which it will then read from the keyboard. The program will
+ then compute the result of performing the specified operation using the operand(s) provided and output 
+ this result. The program should repeat this behavior until the user specifies the q operation.
+	Loop:
+1.	Display MENU via cout.
+2.	Read the operator from cin.
+3.	If the operator is q, terminate the loop. Otherwise continue with the following:
+4.	Display a prompt for the first operand via cout.
+5.	Read the operand from cin.
+6.	If the operator is a binary operator:
+a.	Display a prompt for the second operand via cout.
+b.	Read operand 2 from cin.
+7.	Compute result using operand 1, the operator and operand 2.
+8.	Output result.
+9.	If and invalid operator is entered display an error message and return 0.
 ===============================================================*/
 
 #include <iostream>
