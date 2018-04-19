8365898b9fbdd1f640d804343a5f50970fbe921b
Junya Zhao <31264540+JunyaZ@users.noreply.github.com>
2018-04-19 12:46:48 -0500

    Update practice2_2.cpp

--- practice2_2.cpp
+++ practice2_2.cpp
@@ -1,3 +1,19 @@
+/*******************************************************************
+1.partitions the list using the first element, called a pivot
+2.After the partition, the elements in the list are rearranged 
+so that all the elements before the pivot are less than or equal
+to the pivot and the elements after the pivot are greater than the pivot. 
+The function also returns the index where the pivot is located in the new list. 
+Implement the function in a way that takes size number of comparisons.
+Write a test program that prompts the user to enter a list and displays the list after the partition. Here is a sample run. 
+ps: the first number in the input indicates the number of the elements in the list. Assume the maximum list size is 10.
+========================================================
+looks like
+Enter list: 8 10 1 5 16 61 9 11 1
+After the partition, the list is: 9 1 5 1 10 61 11 16
+=====================================================
+
+*********************************************************************/
 #include <iostream>
 #include <string>
 
